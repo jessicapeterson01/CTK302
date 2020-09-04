339d34cbfb52828c31402e2feadb902350cbc4c1
jessicapeterson01 <70193870+jessicapeterson01@users.noreply.github.com>
2020-09-04 14:08:18 -0500

    my commit for mp1

---
+++ p5/Unit1/mp1/sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+  createCanvas(900, 500) ;
+}
+
+function draw() {
+
+
+
+if (mouseIsPressed) {
+  background(255, 253, 135) ;
+
+} else {
+  background('pink');
+}
+
+// put your shapes here!
+
+ellipse(422, 178, 100, 100) ;
+
+
+
+
+
+text(mouseX + " , " + mouseY, 10, 20) ; //this helps know where the mouse is
+
+
+if (mouseIsPressed) {
+
+
+} else {
+
+ }
+
+}
