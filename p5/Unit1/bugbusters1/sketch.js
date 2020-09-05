71659ad14c66d725b01671939c7f2d400f76e513
jessicapeterson01 <70193870+jessicapeterson01@users.noreply.github.com>
2020-09-05 14:29:39 -0500

    Corrected errors

--- p5/Unit1/bugbusters1/sketch.js
+++ p5/Unit1/bugbusters1/sketch.js
@@ -6,45 +6,41 @@
 
 
 function setup() {
-createCanvas(400, 400);
+  createCanvas(400, 400);
 }
 
 function draw() {
   // the background color
-    if (mouseisPressed) {
-      background('red');
-    } else {
-      background('blue');
-    }
-
-   // this ellipse should be 200x200 pixels in the center of the screen
-    ellipse(200, 200, width/2, height/2);
-
-    // the mouth - smile if pressed, frown otherwise
-    if (mouseIsPressed) {
-      arc(200, 210, 130, 100, 0, PI); // smile - (this line is correct)
-    } else {
-      arc(0, 270 130, 100, PIE, 0) ; // frown
-
-    }
-
-    // the eyes
-    fill('white') ;
-    ellipse(170, 180, 40, 40);
-    ellipse(230, 180, 40, 40);
-
-
-
-
-      if (mouseIsPressed) {
-          print("for debugging")
-          }
-
-          text(mouseX + " , " + mouseY, 10, 20) ; // print mouse locations to canvas
-
-    }
+  if (mouseIsPressed) {
+    background('red');
+  } else {
+    background('blue');
   }
+
+  // this ellipse should be 200x200 pixels in the center of the screen
+  ellipse(200, 200, width / 2, height / 2);
+
+  // the mouth - smile if pressed, frown otherwise
+  if (mouseIsPressed) {
+    arc(200, 210, 130, 100, 0, PI); // smile - (this line is correct)
+  } else {
+    arc(200, 270, 130, 100, PI, 0); // frown
+
   }
+
+  // the eyes
+
+  ellipse(170, 180, 40, 40);
+  ellipse(230, 180, 40, 40);
+
+
+fill('white');
+
+if (mouseIsPressed) {
+  print("for debugging")
+}
+
+text(mouseX + " , " + mouseY, 10, 20); // print mouse locations to canvas
 
 
 
