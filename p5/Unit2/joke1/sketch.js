886741c7f0d1a238a95f55d2fc8f5122a348a662
jessicapeterson01 <70193870+jessicapeterson01@users.noreply.github.com>
2020-09-26 22:21:12 -0500

    joke 1 update

--- p5/Unit2/joke1/sketch.js
+++ p5/Unit2/joke1/sketch.js
@@ -1,23 +1,23 @@
-let state = 0;
+let state = 0 ;
 
 
 function setup() {
-  createCanvas(500, 500);
-  textAlign(CENTER);
-  textSize(26);
+  createCanvas(500, 500) ;
+  textAlign(CENTER) ;
+  textSize(26) ;
 }
 
 function draw() {
 
   switch (state) {
 case 0:
-background('orange');
-text("why didn't the skeleton go\nto see a scary movie?", 250, 250);
+background('orange') ;
+text("why didn't the skeleton go\nto see a scary movie?", 250, 250) ;
 break ;
 
 case 1:
-background('gray');
-text("because he didn't have the guts.", 250, 250);
+background('gray') ;
+text("because he didn't have the guts.", 250, 250) ;
 break ;
 
   }
@@ -28,7 +28,7 @@ function mouseReleased() {
   state++;
 
   if (state > 1) {
-    state = 0;
+    state = 0 ;
 
   }
 
