a0a28cecc83d14c696013533ed486279d522fd29
jessicapeterson01 <70193870+jessicapeterson01@users.noreply.github.com>
2020-09-26 21:55:51 -0500

    joke 1 (why didn't the skeleton go to see...)

--- p5/Unit2/joke1/sketch.js
+++ p5/Unit2/joke1/sketch.js
@@ -11,13 +11,13 @@ function draw() {
 
   switch (state) {
 case 0:
-background('grey');
-text("why did the vampire take an art class?", 250, 250);
+background('orange');
+text("why didn't the skeleton go\nto see a scary movie?", 250, 250);
 break ;
 
 case 1:
-background('red');
-text("because he wanted to learn\nhow to draw blood", 250, 250);
+background('gray');
+text("because he didn't have the guts.", 250, 250);
 break ;
 
   }
